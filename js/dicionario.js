a61ea8c489121f0e1eb569a6a70896672a751c27
Luiz Chequini <luizchequini@gmail.com>
2020-12-22 12:43:47 -0300

    Aula 2 - Array e Dicionários |Criação de Dicionário Simples contendo um unico aluno e Criação de um Dicionário com uma lista de alunos

---
+++ js/dicionario.js
@@ -0,0 +1,32 @@
+var dicionario = 
+{
+    nome: 'Luiz Chequini', 
+    formacao: 'Bacharel em Sistemas de Informação'
+}
+
+var dicionarioLista = 
+[
+    {
+        nome: 'Luiz Chequini', 
+        formacao: 'Bacharel em Sistemas de Informação'
+    },
+    {
+        nome: 'João Antonio', 
+        formacao: 'Bacharel em Sistemas de Informação'
+    },
+    {
+        nome: 'Carlos Ishiba', 
+        formacao: 'Bacharel em Ciências da Computação'
+    },
+]
+
+console.log('Dicionário simples')
+console.log(dicionario);
+console.log(dicionario.nome);
+console.log(dicionario.formacao);
+
+console.log('Dicionário lista')
+console.log(dicionarioLista);
+console.log(dicionarioLista[0].nome);
+console.log(dicionarioLista[1].nome);
+console.log(dicionarioLista[2].nome)
